248479e508881f690361be033abeba203e6e6c52
Josh Taningco <josh.taningco@gmail.com>
2021-09-23 00:58:59 +0800

    added waitlist component

---
+++ src/components/Waitlist/Waitlist.jsx
@@ -0,0 +1,65 @@
+// DEPENDENCIES
+import React from "react";
+import { Waitlist as WaitlistApi } from 'waitlistapi';
+
+// COMPONENTS
+import {
+    Section,
+    Header,
+    HeaderTitle,
+    Title,
+    Span,
+    Star,
+    Caption,
+
+    Container,
+    Wrapper,
+    Card,
+    Sticker,
+    Content,
+    Icon,
+    ContentTitle,
+    Body,
+    Widget
+} from "./styles"
+
+// ASSETS
+import background from "./assets/background.png"
+import star from "./assets/star.png"
+import sticker from "./assets/waitlist.png"
+import icon from "./assets/waitlist_star.png";
+
+const Waitlist = () => (
+    <Section>
+        <Header>
+            <HeaderTitle>
+                <Star src={star} alt="Murch Star" className="left" />
+                <Title className="fredoka-one">
+                    Start your merchandise journey<br />
+                    <Span className="fredoka-one">with murch</Span>.
+                </Title>
+                <Star src={star} alt="Murch Star" className="right" />
+            </HeaderTitle>
+            <Caption>We’ll hop you right onboard once ready, referring to your fellow creators will bump you further up the list!</Caption>
+        </Header>
+
+        <Container style={{ backgroundImage: `url(${background})` }}>
+            <Wrapper>
+                <Sticker src={sticker} alt="Murch Sticker" className="left" />
+                <Card>
+                    <Content>
+                        <Icon src={icon} alt="Star" />
+                        <ContentTitle>Join the Waitlist</ContentTitle>
+                        <Body>Get exclusive access to...</Body>
+                    </Content>
+                    <Widget>
+                        <WaitlistApi api_key="EAYIW2" waitlist_link="https://waitlist.murch.shop/" />
+                    </Widget>
+                </Card>
+                <Sticker src={sticker} alt="Murch Sticker" className="right" />
+            </Wrapper>
+        </Container>
+    </Section>
+)
+
+export default Waitlist;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
-    <Footer />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
   document.getElementById('root')
 );
